d4f191144f8d8242c32058c175d273ae46d05786
matheus678 <60980352+matheus678@users.noreply.github.com>
2020-05-15 20:59:54 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OlaUsuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Olá, "+nome+ "!");
+        }
+    }
+}
